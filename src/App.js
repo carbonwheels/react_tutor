3a16fc27a14a185719009f394195e103fd0fb5c8
raristo <raristo@msn.com>
2019-02-03 14:21:14 -0500

    Routes and Links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Home from './pages/Home';
-import './App.css';
+import { Route, BrowserRouter, Switch, Link } from "react-router-dom";
 
 // NOTES: This will utilize materials themes into the app
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import blue from "@material-ui/core/colors/blue";
 import green from "@material-ui/core/colors/green";
+
+import Home from './pages/Home';
+import Jason from './pages/Jason'
+import './App.css';
 
 const theme = createMuiTheme({
 	palette: {
@@ -22,9 +25,20 @@ class App extends Component {
 
 		return (
 			<MuiThemeProvider theme={theme}>
-				<div className="App">
-					<Home />
-				</div>
+
+				<div style={{ paddingTop: "55px" }} />
+				<BrowserRouter>
+					
+					<div className="App">
+						<Link to="/">Home</Link> <Link to="/jason">Jason</Link>
+						<Switch>
+							<Route path="/Jason" component={Jason} />
+							{/* NOTE: This is the catch all route */}
+							<Route component={ Home } />
+						</Switch>
+					</div>
+				</BrowserRouter>
+
 			</MuiThemeProvider>
 		);
 	}
---
+++ src/pages/Jason.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+class Jason extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<Typography variant="h2" color="secondary">
+					Jason
+          		</Typography>
+			</div>
+		)
+	}
+}
+
+export default Jason;
